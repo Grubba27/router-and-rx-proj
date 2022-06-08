7d97f0ab047c7be1ff8ea9dcaffb60318b107d5e
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2022-06-08 10:56:24 -0300

    added some nesting

--- src/app/foo/foo.component.ts
+++ src/app/foo/foo.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {MatButtonToggleModule} from "@angular/material/button-toggle";
-import {RouterModule} from "@angular/router";
+import {Route, Router, RouterModule, Routes} from "@angular/router";
+import {OtherServiceService} from "../dashboard/services/other-service.service";
 
 @Component({
   selector: 'app-foo',
@@ -14,10 +15,47 @@ import {RouterModule} from "@angular/router";
 })
 export class FooComponent implements OnInit {
 
-  constructor() {
+
+  constructor(
+    private router: Router,
+    private otherService: OtherServiceService
+  ) {
   }
 
   ngOnInit(): void {
   }
 
+
+  async addNested(): Promise<void> {
+    this.otherService.setData(this.otherService.getData().value + 1);
+    const times = this.otherService.getDataAsInArray();
+    const nested: Routes = [{
+      path: ':n',
+      component: FooComponent,
+      children: []
+    }];
+    const handleArrayDepth = (nested: Routes) => {
+      nested?.forEach(({children, path, component}) => {
+        if (children?.length) {
+          handleArrayDepth(children);
+        } else {
+          children?.push({
+            path: ':n',
+            component: FooComponent,
+            children: []
+          });
+        }
+      });
+    };
+
+    handleArrayDepth(nested);
+    console.log(nested);
+    this.router.resetConfig([{
+      path: 'home',
+      component: FooComponent,
+      children: nested
+    }, ...this.router.config]);
+
+    await this.router.navigate(['/home', ...this.otherService.getDataAsInArray()]);
+  }
 }
